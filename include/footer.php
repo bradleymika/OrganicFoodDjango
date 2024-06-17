c75ae4453c1b1c53602b25052c06587a62188b0f
BradleyMika <133171371+bradleymika@users.noreply.github.com>
2024-06-17 14:57:16 -0400

    Add files via upload

---
+++ include/footer.php
@@ -0,0 +1,46 @@
+      <footer class="footer">
+          <div class="container">
+
+
+              <div class="bottom-footer">
+                  <div class="row">
+                      <div class="col-xs-12 col-sm-3 payment-options color-gray">
+                          <h5>Payment Options</h5>
+                          <ul>
+                              <li>
+                                  <a href="#"> <img src="images/paypal.png" alt="Paypal"> </a>
+                              </li>
+                              <li>
+                                  <a href="#"> <img src="images/mastercard.png" alt="Mastercard"> </a>
+                              </li>
+                              <li>
+                                  <a href="#"> <img src="images/maestro.png" alt="Maestro"> </a>
+                              </li>
+                              <li>
+                                  <a href="#"> <img src="images/stripe.png" alt="Stripe"> </a>
+                              </li>
+                              <li>
+                                  <a href="#"> <img src="images/bitcoin.png" alt="Bitcoin"> </a>
+                              </li>
+                          </ul>
+                      </div>
+                      <div class="col-xs-12 col-sm-4 address color-gray">
+                          <h5>Address</h5>
+                          <p>89 Mulberry St, Newark, NJ 07102</p>
+                          <h5>Phone: (+1)9969969696</a></h5>
+                      </div>
+                      <div class="col-xs-12 col-sm-5 additional-info color-gray">
+                          <h5>Addition informations</h5>
+                          <p>Join thousands of other restaurants who benefit from having partnered with us.</p>
+                      </div>
+                  </div>
+              </div>
+
+          </div>
+      </footer>
+
+
+      <!-- /*!
+ 
+
+      */ -->
